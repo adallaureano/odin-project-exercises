1d20d016284145de510fa75051e5eee9070a6a1d
Adalberto Lauriano <laureano.adalberto@gmail.com>
2020-10-21 14:51:12 +0100

    Create caesar_cipher.rb

---
+++ caesar_cipher.rb
@@ -0,0 +1,12 @@
+print "Word please: "
+text = gets.chomp.downcase
+print "Number please: "
+n = gets.chomp.to_i
+
+def caesar_cipher(text, n)
+  alphabet = ('a'..'z').to_a 
+  key = Hash[alphabet.zip(alphabet.rotate(n))]
+  text.each_char.inject("") { |newtext, char| newtext + key[char] }
+end
+
+puts caesar_cipher(text, n)
